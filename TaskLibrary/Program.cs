6e3f896a6ce5b2869f7d5f70af038854ff46ee32
nitin muteja <nitinmuteja@gmail.com>
2020-08-20 12:40:19 +0530

    Added samples for parallel tasks

---
+++ TaskLibrary/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static async Task Main()
+    {
+        // Call Task.Run and invoke Method1.
+        // ... Then call Method2.
+        //     Finally wait for Method2 to finish for terminating the program.
+       Console.WriteLine("Main ThreadId : "+Thread.CurrentThread.ManagedThreadId);
+       var task1= Task.Run(() => Method1());
+       Console.WriteLine("Running in parallel");
+       var task2= Task.Run(() => Method2());
+       Console.WriteLine("Main  ThreadId : "+Thread.CurrentThread.ManagedThreadId);
+       await task1;
+       Console.WriteLine("Main  ThreadId : "+Thread.CurrentThread.ManagedThreadId);
+       await task2;  
+       Console.WriteLine("Main  ThreadId : "+Thread.CurrentThread.ManagedThreadId);
+    }
+
+    static void Method1()
+    {   Console.WriteLine("ThreadId : "+Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(6000);
+        Console.WriteLine("::Method1::");
+    }
+
+    static void Method2()
+    {
+        Console.WriteLine("ThreadId : "+Thread.CurrentThread.ManagedThreadId);
+        Thread.Sleep(6000);
+        Console.WriteLine("::Method2::");
+    }
+}
